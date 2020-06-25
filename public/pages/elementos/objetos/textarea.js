3eba631afad0042379e6155191f16596f39fd367
Mariana Barros <mamirandabarros11@gmail.com>
2020-06-25 09:35:32 -0300

    textarea

--- public/pages/elementos/objetos/textarea.js
+++ public/pages/elementos/objetos/textarea.js
@@ -1,16 +1,16 @@
 export const textarea = (info) => {
-    const textarea = `
-    <textarea type="${info.type}" class="textarea ${info.class}" size="${info.size}" id="${
+  const textarea = `
+    <textarea type="${info.type}" class="textarea ${info.class}" rows="${info.rows}" id="${
     info.id
-  }" placeholder="${info.placeholder}">${info.value || ''}</textarea>`;
-    return textarea;
+    }" placeholder="${info.placeholder}">${info.value || ''}</textarea>`;
+  return textarea;
 };
 
 export default (info) => {
-    const element = `<textarea type="${info.type}" class="textarea ${info.class}" size="${
-    info.size
-  }" id="${info.id}" placeholder="${info.placeholder}">${info.value || ''}</textarea>`;
-    const id = `#${info.id}`;
+  const element = `<textarea type="${info.type}" class="textarea ${info.class}" rows="${
+    info.rows
+    }" id="${info.id}" placeholder="${info.placeholder}">${info.value || ''}</textarea>`;
+  const id = `#${info.id}`;
 
-    return { element, id };
+  return { element, id };
 };
